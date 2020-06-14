ce2ee85d88cd521bca63c743e6cb4c482645f0dc
Devon Martin <devmart10@gmail.com>
2020-06-14 00:12:12 -0700

    adds awesome shit

---
+++ src/components/GroceryItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex items-center p-2">
+    <div
+      class="inline-block w-6 h-6 mr-2 border-2 border-orange-500 rounded-full"
+      :class="{ 'bg-orange-500': item.complete }"
+      @click="complete"
+    ></div>
+    <div>
+      <span class="mr-2 disabled">{{ item.title }}</span>
+      <span class="text-sm text-gray-700">{{ item.quantity ? `${item.quantity}` : "" }}</span>
+    </div>
+    <span class="ml-auto mr-2">{{ item.price ? `$${item.price}` : "-" }}</span>
+    <button class="px-1 text-xs underline" @click="pop">delete</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "GroceryItem",
+  props: ["item"],
+  data: function() {
+    return {
+      editMode: false,
+    };
+  },
+  methods: {
+    complete() {
+      this.$emit("complete");
+    },
+    edit() {
+      this.editMode = true;
+    },
+    pop() {
+      console.log(`pop()`);
+      this.$emit("pop");
+    },
+  },
+  watch: {
+    editMode: {
+      handler(editMode) {
+        console.log(`editMode now ${editMode}`);
+      },
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -1,39 +1,76 @@
 <template>
   <div class="md:mt-32">
     <div class="flex flex-col w-full max-w-3xl p-8 mx-auto mb-4 border rounded-lg">
-      <img alt="Vue logo" src="../assets/shopping_cart.png" class="object-contain h-32" />
-
-      <h1 class="text-3xl">Welcome to Shopping List!</h1>
-      <span>Enter a code</span>
-      <flex>
-        <input type="text" class="mr-2 border" v-model="code" />
-        <button class="px-1 font-bold text-orange-500 border border-orange-500" @click="handleCode">Go</button>
-      </flex>
-      <span>or create a new one!</span>
-      <button class="text-white bg-orange-500" @click="handleCreate">New List</button>
+      <div class="flex items-start">
+        <h1 class="text-3xl leading-loose">Welcome to Shopping List!</h1>
+        <img alt="Vue logo" src="../assets/shopping_cart.png" class="object-contain h-32 ml-auto" />
+      </div>
+      <div class="flex mb-2 leading-loose">
+        <span class="mr-2">Room code:</span>
+        <span class="font-bold tracking-wider">{{code}}</span>
+      </div>
+      <hr class="mb-2" />
+      <div class="flex mb-2">
+        <span class="mr-2">Enter a room code</span>
+        <input type="text" class="px-2 mr-2 border" v-model="userCode" />
+        <button
+          class="px-1 mr-2 font-bold text-orange-500 border border-orange-500"
+          @click="handleCode"
+        >Go</button>
+        <!-- <button
+          class="px-1 text-sm font-bold text-gray-500 underline border-orange-500"
+          @click="handleCopy"
+        >Copy</button>-->
+      </div>
+      <div class="flex">
+        <span class="mr-2">or create a new one!</span>
+        <button class="px-2 text-white bg-orange-500" @click="handleCreate">New List</button>
+      </div>
     </div>
     <!-- --- -->
     <div class="flex flex-col w-full max-w-3xl p-8 mx-auto border rounded-lg">
-      <h1 class="text-3xl leading-loose underline">{{ list.name }}</h1>
-      <!-- <code class="break-all bg-gray-200 border">{{ JSON.stringify(list) }}</code> -->
-      <div v-for="(item, index) in list.items" :key="item.title" class="flex items-center p-2">
-        <div
-          class="inline-block w-6 h-6 mr-2 border-2 border-orange-500 rounded-full"
-          :class="{ 'bg-orange-500': item.complete }"
-          @click="markComplete(index)"
-        ></div>
-        <div>
-          <span class="mr-2 text-xl">{{ item.title }}</span>
-          <span class="text-sm text-gray-700">{{ item.quantity ? `x${item.quantity}` : "" }}</span>
-        </div>
-
-        <span class="ml-auto">{{ item.price ? `$${item.price}` : "-" }}</span>
+      <div class="flex items-baseline px-2 leading-loose">
+        <h2 class="mr-2 text-3xl underline">{{ list.name }}</h2>
+        <span class="text-gray-500">{{ `(${total.completed}/${total.len} complete)` }}</span>
       </div>
-      <div class="flex flex-wrap mt-2">
-        <input type="text" placeholder="New item..." v-model="newItem.title" class="w-auto border" />
-        <input type="text" placeholder="Amount" v-model="newItem.quantity" class="w-16 border" />
-        <input type="text" placeholder="Price" v-model="newItem.price" class="w-16 border" />
-        <button class="px-1 font-bold text-orange-500 border border-orange-500" @click="addNewItem">Add Item</button>
+      <GroceryItem
+        v-for="(item, index) in list.items"
+        :key="item.title"
+        :item="item"
+        @complete="markComplete(index)"
+        @pop="remove(index)"
+      />
+      <div class="flex items-center px-2 leading-loose border-t">
+        <h2 class="ml-auto mr-2">Est. Total: ${{ total.total }}</h2>
+        <span class="text-sm text-gray-500">{{ `(${total.scored}/${total.len})` }}</span>
+      </div>
+      <div class="flex flex-wrap mt-2 md:flex-col">
+        <input
+          type="text"
+          placeholder="New item..."
+          v-model="newItem.title"
+          class="w-auto px-2 mb-2 border md:w-50"
+          @focus="toggleFocus"
+          @blur="toggleFocus"
+        />
+        <button
+          class="flex-grow px-1 mb-2 font-bold text-orange-500 border border-orange-500 md:w-50"
+          @click="addNewItem"
+        >Add Item</button>
+        <div v-if="newItemFocus" class="flex">
+          <input
+            type="text"
+            placeholder="Amount"
+            v-model="newItem.quantity"
+            class="w-full px-2 mb-2 border md:w-50"
+          />
+          <input
+            type="text"
+            placeholder="Price"
+            v-model="newItem.price"
+            class="w-full px-2 mb-2 border md:w-50"
+          />
+        </div>
       </div>
     </div>
     <!-- --- -->
@@ -44,66 +81,93 @@
 import { db } from "../db/db";
 const lists = db.collection("grocery-lists");
 
+import GroceryItem from "./GroceryItem";
+
 export default {
   name: "HomePage",
+  components: {
+    GroceryItem
+  },
   data: function() {
     return {
-      //   code: "Dyu8GaEC1MwyAiwXl1hE",
+      userCode: "",
       code: "test",
       list: {},
       newItem: {
         title: "",
         quantity: "",
-        price: "",
+        price: ""
       },
+      newItemFocus: false
     };
   },
-  //   firestore: {
-  //     documents: db.collection("grocery-lists").doc(),
-  //   },
+  computed: {
+    total() {
+      // total price only including those with price
+      const total = this.list.items.reduce((acc, cur) => {
+        return cur.price ? Number(Number(acc) + Number(cur.price)) : acc;
+      }, 0);
+      // total that have a price TODO: combine into `total` reducer
+      const scored = this.list.items.filter(item => {
+        return item.price && item.price !== "";
+      }).length;
+      // total completed
+      const completed = this.list.items.filter(item => {
+        return item.complete;
+      }).length;
+      let len = this.list.items.length;
+      return { total, scored, completed, len };
+    }
+  },
   watch: {
     code: {
       // call it upon creation too
       immediate: true,
       handler(code) {
+        // add error handling for bad codes
         this.$bind("list", lists.doc(code));
-      },
-    },
+      }
+    }
   },
   methods: {
+    handleCopy() {
+      console.log(`copying...`);
+      // document.execCommand("copy");
+    },
+    toggleFocus() {
+      //   this.newItemFocus = !this.newItemFocus;
+      this.newItemFocus = true;
+    },
+    remove(index) {
+      this.list.items.splice(index, 1);
+      this.$firestoreRefs.list.set(this.list).then(() => {});
+    },
     handleCode() {
-      console.log(`handling code ${this.code}`);
-      console.log(`go to list page...`);
+      this.code = this.userCode;
     },
     async handleCreate() {
-      console.log(`go to new list page...`);
       const ref = await lists.add({
         name: "New List",
-        items: [],
+        items: []
       });
-      console.log(ref);
       this.code = ref.Pc.path.segments[1];
+      this.userCode = "";
     },
     markComplete(index) {
       this.list.items[index].complete = !this.list.items[index].complete;
-
-      this.$firestoreRefs.list.set(this.list).then(() => {
-        console.log("list updated!");
-      });
+      this.$firestoreRefs.list.set(this.list).then(() => {});
     },
     addNewItem() {
       this.list.items.push(this.newItem);
-
       this.$firestoreRefs.list.set(this.list).then(() => {
-        console.log(`added ${this.newItem.title}!`);
         this.newItem = {
           title: "",
           quantity: "",
-          price: "",
+          price: ""
         };
       });
-    },
-  },
+    }
+  }
 };
 </script>
 
